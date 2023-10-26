c3e130bc2df289edccc501f5d3d70cadefc80a8a
D.ALTAI <dawood.altai111@gmail.com>
2023-10-26 11:13:45 +0300

    button added

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let isRecognitionActive = false;
+let recognitionInterval;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+    (stream) => {
+      video.srcObject = stream;
+      startButton.disabled = false;
+    },
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
+async function startRecognition() {
+  isRecognitionActive = true;
+  recognitionInterval = setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const displaySize = { width: video.width, height: video.height };
+    canvas.width = video.width;
+    canvas.height = video.height;
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100);
+}
+
+
+function stopRecognition() {
+  isRecognitionActive = false;
+  clearInterval(recognitionInterval);
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+}
+
+startButton.addEventListener('click', () => {
+  if (!isRecognitionActive) {
+    startRecognition();
+    startButton.style.display = 'none';
+    stopButton.style.display = 'block';
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  if (isRecognitionActive) {
+    stopRecognition();
+    stopButton.style.display = 'none';
+    startButton.style.display = 'block';
+  }
+});
