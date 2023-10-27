944a4be98847d4965553726623577473defd4017
D.ALTAI <dawood.altai111@gmail.com>
2023-10-28 02:00:37 +0300

    Responsivness with the mobile screen is fixed.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Taking the required elements from HTMLm html
+// Taking the required elements from HTML
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
-
 
 let isRecognitionActive = false;
 let recognitionInterval;
@@ -16,7 +15,7 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo);
 
-// Allowing camera to show our face
+// Allowing the camera to show our face
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
@@ -27,8 +26,6 @@ function startVideo() {
     (err) => console.error(err)
   );
 }
-
-// Calling libraries to detection from face-api js and drawing lanmarks 
 
 // Update your startRecognition function to include eye detection
 async function startRecognition() {
@@ -43,66 +40,10 @@ async function startRecognition() {
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-
-    // Additional code to detect eye state
-    resizedDetections.forEach((detection) => {
-      const landmarks = detection.landmarks;
-
-      // Get eye landmarks
-      const leftEye = landmarks.getLeftEye();
-      const rightEye = landmarks.getRightEye();
-
-      // You can set a threshold value for the eye aspect ratio (EAR) to determine open or closed eyes
-      const eyeThreshold = 0.2; // Adjust as needed
-
-      // Detect open or closed eyes
-      const isLeftEyeOpen = isEyeOpen(leftEye, eyeThreshold);
-      const isRightEyeOpen = isEyeOpen(rightEye, eyeThreshold);
-
-      // You can now use isLeftEyeOpen and isRightEyeOpen as indicators of open or closed eyes
-      // You can log or perform actions based on the eye state
-      console.log('Left Eye Open:', isLeftEyeOpen);
-      console.log('Right Eye Open:', isRightEyeOpen);
-    });
   }, 100);
 }
 
-// Function to detect if an eye is open or closed based on the EAR threshold
-function isEyeOpen(eyeLandmarks, threshold) {
-  const topEyelid = (eyeLandmarks[1].y + eyeLandmarks[2].y) / 2;
-  const bottomEyelid = (eyeLandmarks[4].y + eyeLandmarks[5].y) / 2;
-  const eyeHeight = bottomEyelid - topEyelid;
-  const ear = eyeHeight / (eyeLandmarks[3].x - eyeLandmarks[0].x);
-  return ear > threshold;
-}
 
-
-// Function to handle responsive behavior
-function handleResponsiveDesign() {
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth <= 768) {
-    // Adjust elements or behavior for smaller screens (e.g., mobile or tablet)
-    video.width = 320; // Adjust the video width for smaller screens
-    video.height = 240; // Adjust the video height for smaller screens
-  } else {
-    // Reset elements or behavior for larger screens (e.g., desktop)
-    video.width = 720; // Restore the video width for larger screens
-    video.height = 560; // Restore the video height for larger screens
-  }
-}
-
-// Event listener to handle window resize for responsive design
-window.addEventListener('resize', handleResponsiveDesign);
-
-// Event listener to start recognition
-startButton.addEventListener('click', () => {
-  if (!isRecognitionActive) {
-    startRecognition();
-    startButton.style.display = 'none';
-    stopButton.style.display = 'block';
-  }
-});
 
 // Prevent the recognition when the browser is loaded
 function stopRecognition() {
@@ -120,8 +61,7 @@ startButton.addEventListener('click', () => {
   }
 });
 
-//Stop the recognition when click
-
+// Stop the recognition when clicked
 stopButton.addEventListener('click', () => {
   if (isRecognitionActive) {
     stopRecognition();
@@ -129,3 +69,28 @@ stopButton.addEventListener('click', () => {
     startButton.style.display = 'block';
   }
 });
+
+// Function to handle responsive behavior
+
+function handleResponsiveDesign() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight; // Get the screen height as well
+
+  if (screenWidth <= 768) {
+    // Adjust elements for smaller screens (e.g., mobile or tablet)
+    video.width = 412; // Set the video width to 412 for mobile
+    video.height = 300; // Set the video height to 300 for mobile
+    canvas.width = 412; // Set the canvas width to 412 for mobile
+    canvas.height = 300; // Set the canvas height to 300 for mobile
+  } else {
+    // Reset elements for larger screens (e.g., desktop)
+    video.width = 720; // Adjust this as needed for desktop
+    video.height = 560; // Adjust this as needed for desktop
+    canvas.width = 720; // Adjust this as needed for desktop
+    canvas.height = 560; // Adjust this as needed for desktop
+  }
+}
+
+// Event listener to handle window resize for responsive design
+window.addEventListener('resize', handleResponsiveDesign);
+
